1d5bc06bb25865de386a59038ea1c3f7b1c51afb
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-17 08:55:48 +0100

    databases initialized

--- mockAtmApp.py
+++ mockAtmApp.py
@@ -2,3 +2,9 @@
 from datetime import datetime
 import random
 
+# Database
+account_numbers = [4653743234, 236453452, 6322198769, 5687456200]
+allowedUsers = ['Siegfred', 'Kachi', 'Samantha', 'Ojochide']
+allowedPassword = ["passSieg", "passKach", "passSam", "passChide"]
+
+
